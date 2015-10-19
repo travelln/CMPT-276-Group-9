807c552e95d0499eb2ede55d928014b2380d5659
travelln <msg_sunny@msn.com>
2015-10-19 01:27:01 -0700

    login signup complete

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -14,6 +14,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
         password_confirmation: "bar" }
       end
       assert_template 'users/new'
+      assert_select 'div#<CSS id for error explanation>'
+      assert_select 'div.<CSS class for field with error>'
     end
 
 
@@ -26,6 +28,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
           password_confirmation: "password" }
         end
         assert_template 'users/show'
+        # assert_not flash.FILL_IN
+        assert_not flash.nil?
       end
 
 end
