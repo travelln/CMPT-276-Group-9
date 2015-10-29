6caad8aa2a08269c5f80afb3f9cc1852a6fb8921
travelln <msg_sunny@msn.com>
2015-10-29 00:47:33 -0700

    log in user after new user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-
+      log_in @user
       flash[:success] = "Welcome to My Canada"
       redirect_to @user
       # Handle a successful save.
