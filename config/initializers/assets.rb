bf48d65fe5976343fe7301b5426d53137ae5eda4
travelln <msg_sunny@msn.com>
2015-11-02 17:06:57 -0800

    highcharts test 5

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,6 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+Rails.application.config.assets.precompile += %w( jquery-1.4.2.min.js )
+Rails.application.config.assets.precompile += %w( highcharts.js )
