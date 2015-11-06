bda137e61e6f622cdf60897caae3269c9fe88052
niguangfeishiwanjie <zgeng@sfu.ca>
2015-11-05 19:49:06 -0800

    Update static_pages_controller.rb

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -9,6 +9,38 @@ class StaticPagesController < ApplicationController
   end
 
   def crime
+    require 'csv'
+    
+    a=Rails.root.join('app', 'assets', 'Police personnel and selected crime statistics, Canada, provinces and territories.csv')
+    @vec = Array.new
+    i=0
+    CSV.foreach(a, headers:true) do |row|
+      @vec.push(Array.new)
+      row.each do |key, val|
+        if val.to_f == 0.0
+          @vec[i] << val
+        else
+          @vec[i] << val.delete(',').to_f
+        end
+      end
+      i += 1
+    end
+
+    c=Rails.root.join('app', 'assets', 'Crimes, by actual offences.csv')
+    @vec3 = Array.new
+    k=0
+    CSV.foreach(c, headers:true) do |row|
+      @vec3.push(Array.new)
+      row.each do |key, val|
+        if val.to_f == 0.0
+          @vec3[k] << val
+        else
+          @vec3[k] << val.delete(',').to_f
+        end
+      end
+      k += 1
+    end
+
   end
 
   def economic
