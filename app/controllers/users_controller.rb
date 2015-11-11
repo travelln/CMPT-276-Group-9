1c980446f7fc3dd748f24879ce0dd40068a2aeed
travelln <msg_sunny@msn.com>
2015-11-10 21:32:21 -0800

    updated rss feeds for homepage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
-  before_action :admin_user, only: :destroy
+  before_action :admin_user, only: [:destroy, :index]
 
   def show
     @user = User.find(params[:id])
@@ -48,9 +48,6 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
-
-
-
 
 private
   def user_params
