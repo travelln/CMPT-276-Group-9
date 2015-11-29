9c235b49c9ba3d5a91bc4cbb8c9cb1be7fef7e4f
panyuep <panyuep@sfu.ca>
2015-11-28 21:21:44 -0800

    Add some more testings to models

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -8,6 +8,7 @@ class UserTest < ActiveSupport::TestCase
   def setup
     @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
   end
+
   test "should be valid" do
     assert @user.valid?
   end
@@ -26,6 +27,21 @@ class UserTest < ActiveSupport::TestCase
     @user.name = "a" * 51
     assert_not @user.valid?
   end
+
+  test "name should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.name = @user.name.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "name should be lower-case" do
+    bothcase_name = "uSeR"
+    @user.name = bothcase_name
+    @user.save
+    assert_not_equal bothcase_name.downcase, @user.reload.name
+  end
+
   test "email should not be too long" do
     @user.email = "a" * 244 + "@example.com"
     assert_not @user.valid?
@@ -56,9 +72,22 @@ class UserTest < ActiveSupport::TestCase
         assert_not duplicate_user.valid?
       end
 
+
       test "password should have a minimum length" do
         @user.password = @user.password_confirmation = "a" * 5
         assert_not @user.valid?
       end
+
+      test "email addresses should be lower case" do
+        bothcase_email = "uSeR@eXaMPL.cOM"
+        @user.email = bothcase_email
+        @user.save
+        assert_equal bothcase_email.downcase, @user.reload.email
+      end
+
+
+
+
+
 
 end
